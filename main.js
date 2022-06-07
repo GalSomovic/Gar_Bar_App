0a6c9c78514f2bcd711ccdc132914adc32ff63a7
Gal Somovic <galsomovic@gmail.com>
2022-06-07 19:35:07 +0200

    add css

---
+++ main.js
@@ -0,0 +1,14 @@
+document.getElementById('submit_gal').addEventListener('click',submitGal)
+document.getElementById('submit_bar').addEventListener('click',submitBal)
+
+function get(id){
+    return document.getElementById(id).value;
+}
+
+// function submitGal(e){
+//     console.log("gal")
+// }
+//
+// function submitBal(e){
+//     console.log("bar")
+// }
